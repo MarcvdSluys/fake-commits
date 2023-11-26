fbfa8a97b25ba9b2be6875102315f417f26cef24
MvdS <git@vandersluys.nl>
2023-11-26 15:04:20 +0100

    fake-commits: adapt the default from ~100 per day to few per day.
    * fake-commits: adapt the default from ~100 commits per day to few per day:
      + GitHub doesn't seem to like a repo with ~100 commits per day for a number of years;
        - could mask normal commit schedule.
      + Alternative strategy: fill the gaps where you were offline with a 'normal' number of commits.
    
    * Also:
      + split generation of Gaussian random numbers into multiple lines for readability and reuse;
      + allow 0 commits (was: at least a few);
      + add cli options
      	- --no-push: toggle off default pushing to remote;
    	- --pretend: show what would happen, but don't actually DO anything.
      + add some verbosity and comments.

--- fake-commits.py
+++ fake-commits.py
@@ -6,33 +6,34 @@
     2023-12-01, MvdS: initial version.
 """
 
-
-import colored_traceback
+import colored_traceback      # pip install colored_traceback
 colored_traceback.add_hook()
 
 import argparse
-import argcomplete
+import argcomplete            # pip install argcomplete
 
-import numpy as np
+import numpy as np            # pip install numpy
 from pathlib import Path
 import datetime as dt
-from dateutil import rrule
-from git import Repo  # Pip install GitPython
+from dateutil import rrule          # pip install dateutil
+from git import Repo                # pip install GitPython
 import subprocess
 
-import sluyspy.cli as scli     # Pip install sluyspy
+import sluyspy.cli as scli          # pip install sluyspy
 import sluyspy.numerics as snum
-import astrotool.date_time as asdt  # Pip install astrotool
+import astrotool.date_time as asdt  # pip install astrotool
 
 
-# Set the git repo to the current directory:
+# Define your git repo:
 path = '.'
 repo = Repo.init(path).git
 index = Repo.init(path).index
 
-# Specify the two files, and the temporary file:
-file1 = Path('file1')
-file2 = Path('file2')
+# Specify two tiny files, and the temporary file.
+# You need to create file1 and file2 yourself with minimal but different content.
+# I use nearly empty files with '1' and '2' in them.
+file1 = Path('file1')  # e.g. echo 1 > file1
+file2 = Path('file2')  # e.g. echo 2 > file2
 filet = Path('filet')
 
 
@@ -43,7 +44,7 @@ def main():
     # Get command-line arguments:
     args = get_cli_arguments()
     
-    if args.number<1: exit(1)
+    if args.number<=0: exit(1)
     
     # Sort out the date range from the cli arguments:
     first_date, last_date = get_date_range(args)
@@ -56,8 +57,16 @@ def main():
         create_fake_commits(date, args)
     
     # Collect garbage in git repo (gc is not in GitPython):
-    subprocess.run(['git', 'gc', '--aggressive', '--prune=now'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     
+    if args.verbosity>1: print()
+    if args.verbosity>0: print('Tidying up...')
+    if not args.pretend: subprocess.run(['git', 'gc', '--aggressive', '--prune=now'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        
+    # Push to remote:
+    if not args.no_push:
+        if args.verbosity>0: print('Pushing to remote...')
+        if not args.pretend: subprocess.run(['git', 'push'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        
     exit(0)
     
     
@@ -72,13 +81,16 @@ def get_cli_arguments():
     parser = argparse.ArgumentParser(description='Create fake git commits, e.g. to mislead the GitHub Contributions Calendar.',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)  # Use capital, period, add default values
     
-    parser.add_argument('-n', '--number',      type=int, default=10,      help='number of daily commits to create')
-    parser.add_argument('-r', '--random',      action='store_false',      help='do NOT use a random number around the specified number of commits')  # Default = True
+    parser.add_argument('-n', '--number',      type=float, default=2,       help='number of daily commits to create')
+    parser.add_argument('-r', '--random',      type=float, default=1,       help='amount of randomisation around the specified number of commits (0=none)')
     
     parser.add_argument('-f', '--first-date',  type=str, default='today', help='first date to create commits for (YYYY-MM-DD)')
     parser.add_argument('-l', '--last-date',   type=str, default='today', help='last date to create commits for (YYYY-MM-DD)')
     
-    parser.add_argument('-v', '--verbosity', action='count', default=0, help='increase output verbosity')  # Counts number of occurrences
+    parser.add_argument('-p', '--no-push',     action='store_true', help='do NOT push after committing')  # Default = False
+    
+    parser.add_argument('-P', '--pretend',     action='store_true', help='do not actually COMMIT anything')  # Default = False
+    parser.add_argument('-v', '--verbosity',   action='count', default=0, help='increase output verbosity')  # Counts number of occurrences
     
     argcomplete.autocomplete(parser)
     args = parser.parse_args()
@@ -128,17 +140,26 @@ def create_fake_commits(date, args):
       args (struct):  Argparse struct with cli arguments, a.o. the number of commits.
     """
     
-    if args.random:
-        ncommits = snum.nint(max(np.random.normal(args.number,2*np.sqrt(args.number),1)[0],1))
-    else:
-        ncommits = args.number
-        
+    # Draw a single (1) random number from a Gaussian distribution with mean and width specified by the cli
+    # arguments:
+    mean = args.number
+    sqrt_mean = np.sqrt(mean)
+    width = args.random*sqrt_mean
+    ncommits = np.random.normal(mean, width, 1)[0]  # Make it a scalar
+    ncommits = max(snum.nint(ncommits),0)  # Make it an integer
+    # ncommits = snum.nint(max(round(ncommits,0), sqrt_mean))  # Make it an integer, not too small
+    
+    # Report the number of commits for this date:
     if args.verbosity > 0:
         if args.verbosity > 1: print()
-        print('Creating %i fake commits for %s:' % (ncommits, date.strftime('%A %Y-%m-%d')))
-        
-    times = np.sort(np.random.uniform(0,24,ncommits))
+        print('Creating %i fake commits for %s' % (ncommits, date.strftime('%A %Y-%m-%d')), end='')
+        if args.verbosity > 1:
+            print(':')
+        else:
+            print('.')
     
+    # Create random times, sort them and swap files and commit the result for each time:
+    times = np.sort(np.random.uniform(0,24,ncommits))
     for time in times:  # ~1-1.5s for 100 commits
         # Swap file1 and file2:
         file1.rename(filet)
@@ -148,7 +169,8 @@ def create_fake_commits(date, args):
         timestr = asdt.hms_str_from_time(time)
         dtm = dt.datetime.strptime(date.strftime('%Y-%0m-%0d '+timestr), '%Y-%m-%d %H:%M:%S').astimezone()
         if args.verbosity>1: print('  commit time:', dtm)
-        # continue
+        
+        if args.pretend: continue  # If pretending, skip the actual commit
     
         # Commit changes:
         repo.add('file1')
